b63db16f5a4f4c96094520b03ad20a6598fe9c81
Jiwa Chhetri <jiwachhetri14@gmail.com>
2026-08-07 13:05:12 +0100

    Add: longest-substring-without-duplicates - submission-1

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-1.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+
+    lengthOfLongestSubstring(s) {
+        s = s.toLowerCase()
+        if(s.length === 1 || s.length === 0) return s.length;
+
+        let l = 0, r = 1, ans = 1
+        const map = new Map();
+        map.set(s[l])
+
+        while(r < s.length){
+            let _ans = 1
+            while(!map.has(s[r])){
+                map.set(s[r])
+                r++
+                _ans++
+            }
+            l = r
+            r = l + 1
+            ans = Math.max(ans, _ans)
+        }
+
+        return ans
+
+    }
+}
