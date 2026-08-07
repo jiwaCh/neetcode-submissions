6f0027974bb7f30df62e9566c0ab1eed6920e0de
Jiwa Chhetri <jiwachhetri14@gmail.com>
2026-08-07 11:57:42 +0100

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+
+        let lowest = prices[0];
+        let profit = 0;
+
+        for(let i = 0; i < prices.length ; i++){
+            const current = prices[i]
+            if(current < lowest){
+                lowest = current;
+            }
+
+            profit = Math.max(profit, current - lowest)
+
+        }
+        return profit
+    }
+}
