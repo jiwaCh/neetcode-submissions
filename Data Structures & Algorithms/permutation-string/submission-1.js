5b0dda2acfe01f21ac8b08b847af78d8ed1fadf9
Jiwa Chhetri <jiwachhetri14@gmail.com>
2026-08-08 17:59:35 +0100

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {string} s1
+     * @param {string} s2
+     * @return {boolean}
+     */
+    checkInclusion(s1, s2) {
+
+        const map = new Map()
+        for(const s in s1){
+            map.set(s1[s], (map.get(s1[s]) ?? 0) + 1)
+        }
+
+        for(let l = 0, r = 0 ; r < s2.length; r++ ){
+            if(map.get(s2[r]) > 0){
+                map.set(s2[r],map.get(s2[r])-1)
+            }
+            else{
+                if(map.has(s2[l])){
+                    map.set(s2[l],map.get(s2[l])+1)
+                }
+                l++
+            } 
+
+            const empty = [...map.values()].every(item => item <= 0)
+            if(empty) return true
+
+        }
+
+        return false
+
+
+    }
+}
