3955c031b2456cf3c6409fc8c01a8a3a661d87cb
Jiwa Chhetri <jiwachhetri14@gmail.com>
2026-08-10 12:49:18 +0100

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) {
+        const pairs = {
+            "{" : "}",
+            "(" : ")",
+            "[" : "]" 
+        }
+        const stack = []
+        for(let i = 0; i < s.length-1; i++){
+            const current = s[i]
+            if(Object.keys(pairs).includes(current)){
+                stack.push(current)
+            }else{
+                const openingBracket = stack.pop()
+                if(pairs[openingBracket] !== current){
+                    return false
+                }
+            }
+        }
+
+        return Boolean(stack.length)
+    }
+}
